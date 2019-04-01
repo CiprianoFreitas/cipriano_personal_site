bb7d7d4a4390129d1059a893ea3a15dd068591c2
Cipriano Freitas <casf1992@gmail.com>
2019-04-02 00:52:33 +0200

    ➕ added social links (wip)

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,15 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
+          title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
+        <ul style={{ listStyle: `none` }}>
+          <li style={{ display: `inline` }}><a href="//music.cipri.codes"><span aria-hidden="true">🎧</span> Music</a></li>
+          <li style={{ marginLeft: rhythm(1 / 4), display: `inline` }}><a href="//music.cipri.codes"><span aria-hidden="true">🐙</span> Github</a></li>
+          <li style={{ marginLeft: rhythm(1 / 4), display: `inline` }}><a href="//music.cipri.codes"><span aria-hidden="true">🐦</span> Twitter</a></li>
+        </ul>
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug;
           return (
