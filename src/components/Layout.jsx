a68baa33fa9e049b8d46d9b3738f4c0047a1f1ec
Cipriano Freitas <casf1992@gmail.com>
2019-04-25 22:08:59 +0200

    ignore case fix

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from '../styles/global';
+import SEO from './SEO';
+import 'typeface-lora';
+import 'typeface-open-sans';
+
+const Layout = () => (
+    <>
+        <SEO />
+        <GlobalStyle />
+    </>
+);
+
+export default Layout;
