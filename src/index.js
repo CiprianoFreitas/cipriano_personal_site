8ce9bba1c782654ed9e122820373df469af740a2
Cipriano Freitas <casf1992@gmail.com>
2020-03-30 18:57:47 +0200

    adding easter egg

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log(
+    `%c ___________________________________________
+| It looks like you're curious, I like that! |
+| Hit me up with an email at me@cipri.codes  |
+| if you want to share your curiosity with   |
+| me. Don't forget to mention "cow" in the   |
+| email!                                     |
+ -------------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`,
+    'font-family:monospace'
+);
